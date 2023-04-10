073af01dec88f2bf21c4d13a8c12dce9b8d9f1e9
Timothy Murphy <Timmurphy123@gmail.com>
2023-04-10 18:06:37 -0400

    router setup; header and input created

---
+++ vue-todos/src/components/TodoHeader.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <nav class="container-fluid">
+      <div class="branding">
+        <img src="../assets/redCheckMark.png" alt="Big Red Check Mark" width="80" height="80">
+        <h1>To-Do List</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: rgba(255, 255, 255, 0.5);
+
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 200px;
+
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+
+      img {
+        max-width: 50px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+</style>
+
